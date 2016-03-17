54f7c7749ab020987079da776a7f510449ce328b
Erin Kavanaugh <erin@erin-MBA.local>
2016-03-17 12:21:39 -0700

    Completed identity, first, last and each function tests

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -37,6 +38,16 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    var limit = array.length;
+    if (n === 0) {
+      return [];
+    } else if (n === undefined) {
+      return array[limit -1];
+    } else if (n> limit){
+      return array;
+    } else {
+      return array.slice(n - 1, limit);
+    }
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,6 +56,24 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    // if (Array.isArray(collection)) {
+    //   for(var index = 0; index<collection.length; index++) {
+    //     iterator(collection[index], index);
+    //   }
+    // } else {
+    //   for (var key in collection) {
+    //     iterator(collection[key]);
+    //   }
+    // }
+    if(Array.isArray(collection)) {
+      for(var index=0;index<collection.length; index++) {
+        iterator(collection[index], index, collection);
+      }
+    } else {
+      for (var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
